f5af229f4a70fa1a85b635459e914ad8c3daba7d
Ahmad Helal <you@example.com>
2019-07-10 16:05:59 +0300

    3rd task

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+	name= models.CharField(max_length=120)
+	description=models.TextField()
+	opening_time=models.DateTimeField(auto_now_add=False)
+	closing_time=models.DateTimeField(auto_now_add=False)
+	def __str__(self):
+		return self.name
